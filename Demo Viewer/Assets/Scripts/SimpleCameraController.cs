f7242e8bffc04526bf55974d776d40c3c1ec9a70
NtsFranz <ntsluebbers@gmail.com>
2021-07-13 23:51:57 -0400

    old stuff idk just cleaning up

--- Demo Viewer/Assets/Scripts/SimpleCameraController.cs
+++ Demo Viewer/Assets/Scripts/SimpleCameraController.cs
@@ -134,15 +134,6 @@ namespace UnityTemplateProjects
 
         private void Update()
         {
-            // Exit Sample  
-            if (Input.GetKey(KeyCode.Escape))
-            {
-                Application.Quit();
-				#if UNITY_EDITOR
-				UnityEditor.EditorApplication.isPlaying = false; 
-				#endif
-            }
-
             // Hide and lock cursor when right mouse button pressed
             if (Input.GetMouseButtonDown(1))
             {
